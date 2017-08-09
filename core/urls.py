7972e258aa22521d787db23bde0f7b582db2e1d5
solaire-off <victor374288@mail.ru>
2017-08-09 18:38:19 +0300

    Create template (login), work login/logout

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.core.urlresolvers import reverse
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include('django.contrib.auth.urls'), name="logout"),
     url(r'',include('journal.urls')),
 ]
--- journal/views.py
+++ journal/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+
 
 def home(request):
 	context = {}
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+	if request.user.is_authenticated():
+		return render(request,"journal/app.html",context)
 	return render(request,"journal/login.html",context)
 
